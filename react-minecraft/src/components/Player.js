a7df4be488e196a77e42ea8acd2c51e83d8137cc
PieroJacinto <pierojacinto@gmail.com>
2022-12-14 21:45:11 -0300

    move forward, left, right and backward added

--- react-minecraft/src/components/Player.js
+++ react-minecraft/src/components/Player.js
@@ -4,14 +4,12 @@ import { useEffect, useRef } from "react";
 import { Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
-const JUMP_FORCE = 10;
+const JUMP_FORCE = 4;
+const SPEED = 4;
 export const Player = () => {
-  const actions = useKeyboard();
-  // log para ver las teclas que presionamos en consola
-  console.log(
-    "actions",
-    Object.entries(actions).filter(([k, v]) => v)
-  );
+  const { moveBackward, moveForward, moveRight, moveLeft, jump } =
+    useKeyboard();
+
   const { camera } = useThree();
 
   //   creamos una esfera con gravedad y movimiento, el segundo valor de position hace que se cree a 1 de distancia del ground para no collapsar
@@ -42,8 +40,30 @@ export const Player = () => {
     camera.position.copy(
       new Vector3(pos.current[0], pos.current[1], pos.current[2])
     );
-    // seteamos la velocidad
-    if (actions.jump) {
+    // seteamos para movernos en una direccion 
+    const direction = new Vector3()
+    const frontVector = new Vector3(
+      0,
+      0,
+      (moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+    );
+
+    const sideVector = new Vector3(
+      (moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+      0,
+      0
+    );
+
+    direction
+      .subVectors(frontVector,sideVector)
+      .normalize()
+      .multiplyScalar(SPEED)
+      .applyEuler(camera.rotation)
+
+    // ahora aplicamos el vector de velocidad a la esfera, el eje y se queda igual porq estamos moviendonos solo atras y adelante
+    api.velocity.set(direction.x,vel.current[1],direction.z)
+    // seteamos la velocidad y nos fijamos que el personaje este en el suelo para no volver a saltar en el aire indefinidamente
+    if (jump && Math.abs(vel.current[1] < 0.05 )) {
       api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
     }
   });
